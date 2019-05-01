5b71cd72802d7787424b22c64f2628f690fd9b27
olgazelenko <41863842+olgazelenko@users.noreply.github.com>
2019-05-01 14:14:40 +0300

    Update ApplicationTests.java

--- spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
+++ spring-boot-package-war/src/test/java/com/neo/ApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@ComponentScan(basePackages={"com.neo.controller"})
 public class ApplicationTests {
 
 	@Test
